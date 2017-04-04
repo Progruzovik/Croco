76f8ae1ce1ef07b92c5fc9e7957a3739b642f176
Progruzovik <progruzovik@gmail.com>
2017-04-04 20:53:39 +0300

    Добавлена главная страница

--- Croco/src/main/kotlin/net/progruzovik/study/croco/config/WebConfig.kt
+++ Croco/src/main/kotlin/net/progruzovik/study/croco/config/WebConfig.kt
@@ -2,14 +2,17 @@ package net.progruzovik.study.croco.config
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @Configuration
-@EnableWebMvc
 open class WebConfig : WebMvcConfigurerAdapter() {
 
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/api/**").allowedMethods("GET", "POST", "DELETE")
+    }
+
+    override fun addViewControllers(registry: ViewControllerRegistry) {
+        registry.addViewController("/").setViewName("main.html")
     }
 }
